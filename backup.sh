fbcab5e353662e651bf438cbbba2d2bc43ce198e
Shaun Maher <shaun@ghanima.net>
2023-06-05 10:45:29 +0000

    Still drafting the backup script

--- backup.sh
+++ backup.sh
@@ -1,5 +1,11 @@
 #!/usr/bin/bash
 
+OMNIBUS_CONTAINER_NAME="${OMNIBUS_CONTAINER_NAME:-"omnibus"}"
+OPENSSL_PASS="${OPENSSL_PASS:-"pass:mypassword_would_be_here"}"
+
+#Also valid:
+#OPENSSL_PASS="${OPENSSL_PASS:-"file:/path/to/file"}"
+
 sleep $(( $$RANDOM % 3600 ))
 
 apt install -y rclone docker.io
@@ -21,12 +27,14 @@ while true; do
     docker exec omnibus gitlab-backup create INCREMENTAL=yes PREVIOUS_BACKUP=$last_success_start_time; echo $? >/etc/gitlab-backups/current_exit_code;
     incremental_backup=1
   else
-    docker exec omnibus gitlab-backup create; echo $? >/etc/gitlab-backups/current_exit_code;
+    docker exec "${OMNIBUS_CONTAINER_NAME}" gitlab-backup create; echo $? >/etc/gitlab-backups/current_exit_code;
   fi
+
+  # TODO: We also need the secrets file
 
   # TODO: $? isn't set at this point.  Need to load from /etc/gitlab-backups/current_exit_code
   if [ $? -eq 0 ]; then
-    rclone #TODO
+    # pv TODO | openssl enc -aes-512-cbc -md sha512 -pass "${OPENSSL_PASS}" | rclone TODO
 
     if [ $? -eq 0 ]; then
       if [ $incremental_backup -gt 0 ]; then
@@ -34,11 +42,12 @@ while true; do
       else
         cat /etc/gitlab-backups/current_start_time > /etc/gitlab-backups/last_full_success_start_time
       fi
+
+      # TODO: cleanup local files
+
+      # TODO: cleanup remote files
     fi
   fi
 
-  # TODO: cleanup local files
-
-  # TODO: cleanup remote files
   sleep 3600
 done
