f8dd1047d66190343c431cc51a04f0bc54f50db4
Shaun Maher <shaun@ghanima.net>
2023-06-07 08:44:40 +0000

    backup script

--- backup.sh
+++ backup.sh
@@ -8,22 +8,29 @@ S3_ACCESS_KEY="${S3_ACCESS_KEY}"
 S3_SECRET_KEY="${S3_SECRET_KEY}"
 S3_REGION="${S3_REGION:-"us-east-1"}"
 S3_ENDPOINT="${S3_ENDPOINT:-"s3.wasabisys.com"}"
+S3_BUCKET="${S3_BUCKET:-"backups"}"
 GITLAB_BACKUPS_DIR="${GITLAB_BACKUPS_DIR:-"/var/backups/gitlab"}"
+
+RED='\033[1;31m'
+GREEN='\033[1;32m'
+YELLOW='\033[1;33m'
+GRAY='\033[1;90m'
+NC='\033[0m' # No Color
 
 #sleep $(( $RANDOM % 3600 ))
 
-apt install -y rclone docker.io
+apt install -y rclone docker.io pv
 
 function verbose() {
-  printf '%b\n' "${1}"
+  printf '%b\n' "${YELLOW}${1}${NC}"
 }
 
 function debug() {
-  printf '%b\n' "${1}"
+  printf '%b\n' "${GRAY}${1}${NC}"
 }
 
 function info() {
-  printf '%b\n' "${1}"
+  printf '%b\n' "${GREEN}${1}${NC}"
 }
 
 while true; do
@@ -50,10 +57,10 @@ while true; do
   last_success_age=$(( $(date +%s) - $last_success_start_time ))
   last_full_age=$(( $(date +%s) - $last_full_success_start_time ))
 
-  verbose "last_full_success_start_time: $last_full_success_start_time"
-  verbose "last_incremental_success_start_time: $last_incremental_success_start_time"
-  verbose "last_success_start_time: $last_success_start_time"
-  verbose "last_success_age: $last_success_age"
+  debug "last_full_success_start_time: $last_full_success_start_time"
+  debug "last_incremental_success_start_time: $last_incremental_success_start_time"
+  debug "last_success_start_time: $last_success_start_time"
+  debug "last_success_age: $last_success_age"
   date +%s > /etc/gitlab-backups/current_start_time
 
   # Write Wasabi configuration to rclone conf file
@@ -73,19 +80,19 @@ storage_class =
 no_check_bucket = true
 EOF
 
-  find "${GITLAB_BACKUPS_DIR}" | grep -v '.tar$'> /tmp/file_list_before
+  find "${GITLAB_BACKUPS_DIR}" > /tmp/file_list_before
   
-  if [ $last_success_age -gt 3600 ]; then
+  if [ $last_success_age -gt 86400 ]; then
     if [ $last_full_age -lt 2419200 ]; then # 4 weeks
       # We pull the last_success_start_time backwards by 10 minutes just to be
       #  sure this backup and the previous backup overlap and don't miss
       #  anything.
       info "Starting a GitLab Incremental Backup.  PREVIOUS_BACKUP=$(( $last_success_start_time - 600 ))"
-      docker exec omnibus gitlab-backup create INCREMENTAL=yes PREVIOUS_BACKUP=$(( $last_success_start_time - 600 )); echo $? >/etc/gitlab-backups/current_exit_code;
+      docker exec "${OMNIBUS_CONTAINER_NAME}" gitlab-backup create SKIP=registry,artifacts,packages INCREMENTAL=yes PREVIOUS_BACKUP=$(( $last_success_start_time - 600 )); echo $? >/etc/gitlab-backups/current_exit_code;
       incremental_backup=1
     else
       info "Starting a GitLab Full Backup."
-      docker exec "${OMNIBUS_CONTAINER_NAME}" gitlab-backup create; echo $? >/etc/gitlab-backups/current_exit_code;
+      docker exec "${OMNIBUS_CONTAINER_NAME}" gitlab-backup create SKIP=registry,artifacts,packages ; echo $? >/etc/gitlab-backups/current_exit_code;
     fi
     current_backup_result=$(cat /etc/gitlab-backups/current_exit_code)
   else
@@ -93,7 +100,7 @@ EOF
     current_backup_result=$(cat /etc/gitlab-backups/current_exit_code)
   fi
 
-  # TODO: We also need the secrets file
+  # TODO: We also need the secrets files gitlab.rb and gitlab-secrets.json
 
   find "${GITLAB_BACKUPS_DIR}" > /tmp/file_list_after
   IFS=$'\n' read -d '' -r -a new_files < <(diff -ruN /tmp/file_list_before /tmp/file_list_after | grep -v '^\+++' | grep '^\+')
@@ -104,8 +111,8 @@ EOF
       rel_file=$(basename "${abs_file}")
       if [ ! "" == "${file}" ]; then
         verbose "New file was created: ${file}"
-        verbose "pv \"${abs_file}\" | openssl enc -aes-512-cbc -md sha512 -pass \"${OPENSSL_PASS}\" | rclone --config /tmp/rclone.conf rcat \"wasabi:backups.git.ghanima.net/${rel_file}\""
-        pv "${abs_file}" | openssl enc -aes-512-cbc -md sha512 -pass "${OPENSSL_PASS}" | rclone --config /tmp/rclone.conf rcat "wasabi:backups.git.ghanima.net/${rel_file}"
+        debug "pv \"${abs_file}\" | openssl enc -aes-256-cbc -md sha512 -iter 8192000 -pass \"${OPENSSL_PASS}\" | rclone --config /tmp/rclone.conf rcat \"wasabi:${S3_BUCKET}/${rel_file}\""
+        pv "${abs_file}" | openssl enc -aes-256-cbc -md sha512 -iter 8192000 -pass "${OPENSSL_PASS}" | rclone --config /tmp/rclone.conf rcat "wasabi:${S3_BUCKET}/${rel_file}"
         current_copy_exit_code=0 #$(( TODO: $PIPESTATUS[0]??? ))
       fi
     done
@@ -121,10 +128,10 @@ EOF
 
       # TODO: cleanup remote files
     else
-      printf '%s\n' "Backup process failed.  Not uploading files."
+      error "Backup process failed.  Not uploading files."
     fi
   fi
 
-  printf '%b\n' "Backup process complete.  Will now take a nap."
+  info "Backup process complete.  Will now take a nap."
   sleep 3600
 done
